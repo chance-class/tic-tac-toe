9cbf431def2b7bd5180717aca8716beb32feb10a
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-07-01 16:21:11 -0500

    Add function to mark the board and alternating turns for players

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const gameBoard = (function() {
-  const board = ["X", "O"];
-  const addToBoard = (marker) => {
-    board += marker;
-  }
-  return {board, addToBoard};
+  const board = [];
+  return {board};
 })();
 
-const newPlayer = (name, marker) => {
-  return {name, marker};
+const player = (name, marker) => {
+  const addToBoard = (marker) => {
+    gameBoard.board.push(`${marker}`);
+  }
+  return {name, marker, addToBoard};
 }
 
 const displayController = (function() {
   document.addEventListener("click", (e) => {
     if (!(e.target.closest(".quadrant"))) {
       return;
+    } else if (e.target.textContent != "") {
+      return;
     } else {
-      //function to add marker to gameBoard.board
+      if (currentPlayer === playerOne) {
+        currentPlayer = playerTwo;
+      } else if (currentPlayer === playerTwo) {
+        currentPlayer = playerOne;
+      }
+      currentPlayer.addToBoard(currentPlayer.marker);
       const value = (gameBoard.board.length - 1);
       e.target.textContent = gameBoard.board[`${value}`]
     }
   });  
   
 })();
+
+const playerOne = player("Jeff", "X");
+const playerTwo = player("Manny", "O");
+let currentPlayer = playerTwo;
