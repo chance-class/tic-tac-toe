67951e50e3ef88eed0839e1822226bc2ddaf32ce
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-07-02 14:04:13 -0500

    Edit player fact func and rewrite checkForWinner function

--- script.js
+++ script.js
@@ -1,67 +1,61 @@
 const gameBoard = (function() {
-  const board = [];
+  const board = ["", "", "", "", "", "", "", "", ""];
   const getBoard = () => board;
   return {getBoard};
 })();
 
 const player = (name, marker) => {
-  const addToBoard = (marker) => {
+  let x = [], o = [];
+  const addToBoard = (marker, quad) => {
     gameBoard.getBoard().push(`${marker}`);
+    if (marker === "X") {
+      x.push(quad);
+    } else if (marker === "O") {
+      o.push(quad);
+    }
+    console.log(x);
+    console.log(o);
   }
-  return {name, marker, addToBoard};
+  return {name, marker, addToBoard, x, o};
 }
 
 const playerOne = player("Player One", "X");
 const playerTwo = player("Player Two", "O");
 
 const displayController = (function() {
-  const quad1 = document.getElementById("quad-1");
-  const quad2 = document.getElementById("quad-2");
-  const quad3 = document.getElementById("quad-3");
-  const quad4 = document.getElementById("quad-4");
-  const quad5 = document.getElementById("quad-5");
-  const quad6 = document.getElementById("quad-6");
-  const quad7 = document.getElementById("quad-7");
-  const quad8 = document.getElementById("quad-8");
-  const quad9 = document.getElementById("quad-9");
+//  const quad1 = document.getElementById("quad-1");
+//  const quad2 = document.getElementById("quad-2");
+//  const quad3 = document.getElementById("quad-3");
+//  const quad4 = document.getElementById("quad-4");
+//  const quad5 = document.getElementById("quad-5");
+//  const quad6 = document.getElementById("quad-6");
+//  const quad7 = document.getElementById("quad-7");
+//  const quad8 = document.getElementById("quad-8");
+//  const quad9 = document.getElementById("quad-9");
   let gameOver = false;
   const checkForWinner = () => {
-    switch (currentPlayer.marker) {
-        case (quad1.textContent && quad4.textContent && quad7.textContent):
-          alert(`${currentPlayer.name} has won!`);
-          gameOver = true;
-          break;
-        case (quad1.textContent && quad2.textContent && quad3.textContent):
-          alert(`${currentPlayer.name} has won!`);
-          gameOver = true;
-          break;
-        case (quad1.textContent && quad5.textContent && quad9.textContent):
-          alert(`${currentPlayer.name} has won!`);
-          gameOver = true;
-          break;
-        case (quad2.textContent && quad5.textContent && quad8.textContent):
-          alert(`${currentPlayer.name} has won!`);
-          gameOver = true;
-          break;
-        case (quad3.textContent && quad6.textContent && quad9.textContent):
-          alert(`${currentPlayer.name} has won!`);
-          gameOver = true;
-          break;
-        case (quad4.textContent && quad5.textContent && quad6.textContent):
-          alert(`${currentPlayer.name} has won!`);
-          gameOver = true;
-          break;
-        case (quad7.textContent && quad8.textContent && quad9.textContent):
-          alert(`${currentPlayer.name} has won!`);
-          gameOver = true;
-          break;
-        case (quad3.textContent && quad5.textContent && quad7.textContent):
-          alert(`${currentPlayer.name} has won!`);
-          gameOver = true;
-          break;
-        default:
-          return;
-      }
+    let winner = "";
+    const includesAll = (arr, values) => values.every(v => arr.includes(v));
+    if (includesAll(playerOne.x, ["quad-1", "quad-2", "quad-3"]) || includesAll(playerOne.x, ["quad-4", "quad-5", "quad-6"]) || 
+    includesAll(playerOne.x, ["quad-7", "quad-8", "quad-9"]) || includesAll(playerOne.x, ["quad-1", "quad-4", "quad-7"]) || 
+    includesAll(playerOne.x, ["quad-2", "quad-5", "quad-8"]) || includesAll(playerOne.x, ["quad-3", "quad-6", "quad-9"]) ||
+    includesAll(playerOne.x, ["quad-1", "quad-5", "quad-9"]) || includesAll(playerOne.x, ["quad-3", "quad-5", "quad-7"])) {
+      winner = playerOne;
+      alert(`${playerOne.name} wins the game!`);
+    } 
+    if (winner !== playerOne)
+    if (includesAll(playerTwo.o, ["quad-1", "quad-2", "quad-3"]) || includesAll(playerTwo.o, ["quad-4", "quad-5", "quad-6"]) || 
+    includesAll(playerTwo.o, ["quad-7", "quad-8", "quad-9"]) || includesAll(playerTwo.o, ["quad-1", "quad-4", "quad-7"]) || 
+    includesAll(playerTwo.o, ["quad-2", "quad-5", "quad-8"]) || includesAll(playerTwo.o, ["quad-3", "quad-6", "quad-9"]) ||
+    includesAll(playerTwo.o, ["quad-1", "quad-5", "quad-9"]) || includesAll(playerTwo.o, ["quad-3", "quad-5", "quad-7"])) {
+      winner = playerTwo;
+      alert(`${playerTwo.name} wins the game!`);
+    }
+
+    else if (playerOne.x.length === 5 || playerTwo.o.length === 5) {
+      alert("It's a tie!");
+    }
+    
   }
   let currentPlayer = playerOne;
   document.addEventListener("click", (e) => {
@@ -70,10 +64,11 @@ const displayController = (function() {
     } else if (e.target.textContent != "" || gameOver === true) {
       return;
     } else {
-      currentPlayer.addToBoard(currentPlayer.marker);
+      quad = e.target.id;
+      currentPlayer.addToBoard(currentPlayer.marker, quad);
       const value = (gameBoard.getBoard().length - 1);
       e.target.textContent = gameBoard.getBoard()[`${value}`];
-      checkForWinner();
+      checkForWinner(currentPlayer.marker);
       currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
     }
   });  
