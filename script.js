24ad66386dacd3eb3822298182cee93cd2a2ee2f
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-07-05 17:50:58 -0500

    Add offensive parameter to evil computer func

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const gameBoard = (function() {
+const gameBoard = (function () {
   let board = [];
   const getBoard = () => board;
   const clearBoard = () => board.splice(0, 9);
-  return {getBoard, clearBoard};
+  return { getBoard, clearBoard };
 })();
 
 const player = (name, marker) => {
   let x = [], o = [];
   const addToBoard = (marker, quad) => {
-      gameBoard.getBoard().push(`${marker}`);
-      if (marker === "X") {
-        x.push(quad);
-      } else if (marker === "O") {
-        o.push(quad);
-      }
+    gameBoard.getBoard().push(`${marker}`);
+    if (marker === "X") {
+      x.push(quad);
+    } else if (marker === "O") {
+      o.push(quad);
+    }
 
   }
-  return {name, marker, addToBoard, x, o};
+  return { name, marker, addToBoard, x, o };
 }
 
-const displayController = (function() {
+const displayController = (function () {
   const quad1 = document.getElementById("quad-1");
   const quad2 = document.getElementById("quad-2");
   const quad3 = document.getElementById("quad-3");
@@ -31,86 +31,86 @@ const displayController = (function() {
   const quad9 = document.getElementById("quad-9");
 
   const playerOne = player("Player One", "X");
-const playerTwo = player("Player Two", "O");
-let currentPlayer = playerOne;
+  const playerTwo = player("Player Two", "O");
+  let currentPlayer = playerOne;
 
   const header = document.getElementById("header");
-const headDiv = document.createElement("div");
-headDiv.classList.add("head");
-header.appendChild(headDiv);
-document.querySelector(".head").style.fontSize = "20px";
-document.querySelector(".head").style.textAlign = "center";
-headDiv.textContent = "Player One's turn";
+  const headDiv = document.createElement("div");
+  headDiv.classList.add("head");
+  header.appendChild(headDiv);
+  document.querySelector(".head").style.fontSize = "20px";
+  document.querySelector(".head").style.textAlign = "center";
+  headDiv.textContent = "Player One's turn";
 
-const change1 = document.getElementById("change-1");
-const player1 = document.querySelector("#player1");
-change1.addEventListener("click", () => {
-  playerOne.name = prompt("Please enter your name.");
-  if (playerOne.name === null) {
-    playerOne.name = "Player One";
-  } else if (playerOne.name === "Robocop" || playerOne.name === "Dr. Evil") {
-    alert("Please choose a different name.");
-    playerOne.name = "Player One";
-  }
-  player1.textContent = `${playerOne.name}`;
-  if (playerOne === currentPlayer) {
-    headDiv.textContent = `${playerOne.name}'s turn`;
-  }
-})
+  const change1 = document.getElementById("change-1");
+  const player1 = document.querySelector("#player1");
+  change1.addEventListener("click", () => {
+    playerOne.name = prompt("Please enter your name.");
+    if (playerOne.name === null) {
+      playerOne.name = "Player One";
+    } else if (playerOne.name === "Robocop" || playerOne.name === "Dr. Evil") {
+      alert("Please choose a different name.");
+      playerOne.name = "Player One";
+    }
+    player1.textContent = `${playerOne.name}`;
+    if (playerOne === currentPlayer) {
+      headDiv.textContent = `${playerOne.name}'s turn`;
+    }
+  })
 
-const change2 = document.getElementById("change-2");
-const player2 = document.querySelector("#player2");
-change2.addEventListener("click", () => {
-  playerTwo.name = prompt("Please enter your name.");
-  if (playerTwo.name === null) {
-    playerTwo.name = "Player Two";
-  } else if (playerTwo.name === "Robocop" || playerTwo.name === "Dr. Evil") {
-    alert("Please choose a different name.");
-    playerTwo.name = "Player Two";
-  }
-  player2.textContent = `${playerTwo.name}`;
-  if (playerTwo === currentPlayer) {
-    headDiv.textContent = `${playerTwo.name}'s turn`;
-  }
-})
+  const change2 = document.getElementById("change-2");
+  const player2 = document.querySelector("#player2");
+  change2.addEventListener("click", () => {
+    playerTwo.name = prompt("Please enter your name.");
+    if (playerTwo.name === null) {
+      playerTwo.name = "Player Two";
+    } else if (playerTwo.name === "Robocop" || playerTwo.name === "Dr. Evil") {
+      alert("Please choose a different name.");
+      playerTwo.name = "Player Two";
+    }
+    player2.textContent = `${playerTwo.name}`;
+    if (playerTwo === currentPlayer) {
+      headDiv.textContent = `${playerTwo.name}'s turn`;
+    }
+  })
 
-const robocop = document.getElementById("robocop");
-robocop.addEventListener("click", () => {
-  if (robocop.textContent === "Play Computer") {
-    playerTwo.name = "Robocop";
-    player2.textContent = "Robocop";
-    isRobot();
-    robocop.textContent = "Play User";
-  } else {
-    playerTwo.name = "Player Two";
-    player2.textContent = "Player Two";
-    robocop.textContent = "Play Computer";
-  }
-})
+  const robocop = document.getElementById("robocop");
+  robocop.addEventListener("click", () => {
+    if (robocop.textContent === "Play Computer") {
+      playerTwo.name = "Robocop";
+      player2.textContent = "Robocop";
+      isRobot();
+      robocop.textContent = "Play User";
+    } else {
+      playerTwo.name = "Player Two";
+      player2.textContent = "Player Two";
+      robocop.textContent = "Play Computer";
+    }
+  })
 
-const drEvil = document.getElementById("drEvil");
-drEvil.addEventListener("click", () => {
-  if (drEvil.textContent === "Play Evil Computer") {
-    playerTwo.name = "Dr. Evil";
-    player2.textContent = "Dr. Evil";
-    isEvilRobot();
-    drEvil.textContent = "Play User";
-  } else {
-    playerTwo.name = "Player Two";
-    player2.textContent = "Player Two";
-    drEvil.textContent = "Play Evil Computer";
-  }
-})
+  const drEvil = document.getElementById("drEvil");
+  drEvil.addEventListener("click", () => {
+    if (drEvil.textContent === "Play Evil Computer") {
+      playerTwo.name = "Dr. Evil";
+      player2.textContent = "Dr. Evil";
+      isEvilRobot();
+      drEvil.textContent = "Play User";
+    } else {
+      playerTwo.name = "Player Two";
+      player2.textContent = "Player Two";
+      drEvil.textContent = "Play Evil Computer";
+    }
+  })
 
 
   let gameOver = false;
   const checkForWinner = () => {
     let winner = "";
     const includesAll = (arr, values) => values.every(v => arr.includes(v));
-    if (includesAll(playerOne.x, ["quad-1", "quad-2", "quad-3"]) || includesAll(playerOne.x, ["quad-4", "quad-5", "quad-6"]) || 
-    includesAll(playerOne.x, ["quad-7", "quad-8", "quad-9"]) || includesAll(playerOne.x, ["quad-1", "quad-4", "quad-7"]) || 
-    includesAll(playerOne.x, ["quad-2", "quad-5", "quad-8"]) || includesAll(playerOne.x, ["quad-3", "quad-6", "quad-9"]) ||
-    includesAll(playerOne.x, ["quad-1", "quad-5", "quad-9"]) || includesAll(playerOne.x, ["quad-3", "quad-5", "quad-7"])) {
+    if (includesAll(playerOne.x, ["quad-1", "quad-2", "quad-3"]) || includesAll(playerOne.x, ["quad-4", "quad-5", "quad-6"]) ||
+      includesAll(playerOne.x, ["quad-7", "quad-8", "quad-9"]) || includesAll(playerOne.x, ["quad-1", "quad-4", "quad-7"]) ||
+      includesAll(playerOne.x, ["quad-2", "quad-5", "quad-8"]) || includesAll(playerOne.x, ["quad-3", "quad-6", "quad-9"]) ||
+      includesAll(playerOne.x, ["quad-1", "quad-5", "quad-9"]) || includesAll(playerOne.x, ["quad-3", "quad-5", "quad-7"])) {
       gameOver = true;
       headDiv.textContent = `${playerOne.name} wins the game!`;
       const refresh = document.createElement("button");
@@ -140,10 +140,10 @@ drEvil.addEventListener("click", () => {
       })
     }
 
-    else if (includesAll(playerTwo.o, ["quad-1", "quad-2", "quad-3"]) || includesAll(playerTwo.o, ["quad-4", "quad-5", "quad-6"]) || 
-    includesAll(playerTwo.o, ["quad-7", "quad-8", "quad-9"]) || includesAll(playerTwo.o, ["quad-1", "quad-4", "quad-7"]) || 
-    includesAll(playerTwo.o, ["quad-2", "quad-5", "quad-8"]) || includesAll(playerTwo.o, ["quad-3", "quad-6", "quad-9"]) ||
-    includesAll(playerTwo.o, ["quad-1", "quad-5", "quad-9"]) || includesAll(playerTwo.o, ["quad-3", "quad-5", "quad-7"])) {
+    else if (includesAll(playerTwo.o, ["quad-1", "quad-2", "quad-3"]) || includesAll(playerTwo.o, ["quad-4", "quad-5", "quad-6"]) ||
+      includesAll(playerTwo.o, ["quad-7", "quad-8", "quad-9"]) || includesAll(playerTwo.o, ["quad-1", "quad-4", "quad-7"]) ||
+      includesAll(playerTwo.o, ["quad-2", "quad-5", "quad-8"]) || includesAll(playerTwo.o, ["quad-3", "quad-6", "quad-9"]) ||
+      includesAll(playerTwo.o, ["quad-1", "quad-5", "quad-9"]) || includesAll(playerTwo.o, ["quad-3", "quad-5", "quad-7"])) {
       gameOver = true;
       headDiv.textContent = `${playerTwo.name} wins the game!`;
       const refresh = document.createElement("button");
@@ -172,7 +172,7 @@ drEvil.addEventListener("click", () => {
         isEvilRobot();
       })
     }
-    
+
     else if (playerOne.x.length === 5 || playerTwo.o.length === 5) {
       headDiv.textContent = "It's a tie!";
       gameOver = true;
@@ -202,14 +202,14 @@ drEvil.addEventListener("click", () => {
         isEvilRobot();
       })
     }
-    
+
   }
-  
+
   const isRobot = () => {
     if (playerTwo.name === "Robocop") {
       if (playerTwo === currentPlayer) {
-        const choices = ["quad-1", "quad-2", "quad-3", "quad-4", "quad-5", "quad-6", 
-        "quad-7", "quad-8", "quad-9"];
+        const choices = ["quad-1", "quad-2", "quad-3", "quad-4", "quad-5", "quad-6",
+          "quad-7", "quad-8", "quad-9"];
         roboChoice = choices[Math.floor(Math.random() * choices.length)];
         let quad = document.querySelector(`#${roboChoice}`);
         if (quad.textContent != "") {
@@ -233,16 +233,50 @@ drEvil.addEventListener("click", () => {
   const isEvilRobot = () => {
     if (playerTwo.name === "Dr. Evil") {
       if (playerTwo === currentPlayer) {
-        const choices = ["quad-1", "quad-2", "quad-3", "quad-4", "quad-5", "quad-6", 
-        "quad-7", "quad-8", "quad-9"];
-        if (quad1.textContent === "X" && quad3.textContent === "" && quad7.textContent === "" && quad9.textContent === "" || 
+        const choices = ["quad-1", "quad-2", "quad-3", "quad-4", "quad-5", "quad-6",
+          "quad-7", "quad-8", "quad-9"];
+        let roboChoice = "";
+        if (quad1.textContent === "O" && quad2.textContent === "O" && quad3.textContent === "" || 
+        quad6.textContent === "O" && quad9.textContent === "O" && quad3.textContent === "" || 
+        quad5.textContent === "O" && quad7.textContent === "O" && quad3.textContent === "") {
+          roboChoice = "quad-3";
+        } else if (quad2.textContent === "O" && quad3.textContent === "O" && quad1.textContent === "" || 
+        quad4.textContent === "O" && quad7.textContent === "O" && quad1.textContent === "" || 
+        quad9.textContent === "O" && quad5.textContent === "O" && quad1.textContent === "") {
+          roboChoice = "quad-1";
+        } else if (quad1.textContent === "O" && quad4.textContent === "O" && quad7.textContent === "" || 
+        quad8.textContent === "O" && quad9.textContent === "O" && quad7.textContent === "" || 
+        quad3.textContent === "O" && quad5.textContent === "O" && quad7.textContent === "") {
+          roboChoice = "quad-7";
+        } else if (quad3.textContent === "O" && quad6.textContent === "O" && quad9.textContent === "" || 
+        quad7.textContent === "O" && quad8.textContent === "O" && quad9.textContent === "" || 
+        quad1.textContent === "O" && quad5.textContent === "O" && quad9.textContent === "") {
+          roboChoice = "quad-9";
+        } else if (quad1.textContent === "O" && quad9.textContent === "O" && quad5.textContent === "" || 
+        quad3.textContent === "O" && quad7.textContent === "O" && quad5.textContent === "" || 
+        quad4.textContent === "O" && quad6.textContent === "O" && quad5.textContent === "" || 
+        quad2.textContent === "O" && quad8.textContent === "O" && quad5.textContent === "") {
+          roboChoice = "quad-5";
+        } else if (quad1.textContent === "O" && quad3.textContent === "O" && quad2.textContent === "" || 
+        quad5.textContent === "O" && quad8.textContent === "O" && quad2.textContent === "") {
+          roboChoice = "quad-2";
+        } else if (quad3.textContent === "O" && quad9.textContent === "O" && quad6.textContent === "" || 
+        quad4.textContent === "O" && quad5.textContent === "O" && quad6.textContent === "") {
+          roboChoice = "quad-6";
+        } else if (quad1.textContent === "O" && quad7.textContent === "O" & quad4.textContent === "" | 
+        quad5.textContent === "O" && quad6.textContent === "O" && quad4.textContent === "") {
+          roboChoice = "quad-4";
+        } else if (quad7.textContent === "O" && quad9.textContent === "O" && quad8.textContent === "" || 
+        quad2.textContent === "O" && quad5.textContent === "O" && quad8.textContent === "") {
+          roboChoice = "quad-8";
+
+        } else if (quad1.textContent === "X" && quad3.textContent === "" && quad7.textContent === "" && quad9.textContent === "" || 
         quad1.textContent === "" && quad3.textContent === "X" && quad7.textContent === "" && quad9.textContent === "" || 
         quad1.textContent === "" && quad3.textContent === "" && quad7.textContent === "X" && quad9.textContent === "" || 
         quad1.textContent === "" && quad3.textContent === "" && quad7.textContent === "" && quad9.textContent === "X") {
           const moves = ["quad-1", "quad-3", "quad-7", "quad-9", "quad-5"];
           roboChoice = moves[Math.floor(Math.random() * moves.length)];
-        }
-        else if (quad1.textContent === "X" && quad2.textContent === "X" && quad3.textContent === "" || 
+        } else if (quad1.textContent === "X" && quad2.textContent === "X" && quad3.textContent === "" || 
         quad6.textContent === "X" && quad9.textContent === "X" && quad3.textContent === "" || 
         quad5.textContent === "X" && quad7.textContent === "X" && quad3.textContent === "") {
           roboChoice = "quad-3";
@@ -269,7 +303,7 @@ drEvil.addEventListener("click", () => {
         } else if (quad3.textContent === "X" && quad9.textContent === "X" && quad6.textContent === "" || 
         quad4.textContent === "X" && quad5.textContent === "X" && quad6.textContent === "") {
           roboChoice = "quad-6";
-        } else if (quad1.textContent === "X" && quad7.textContent === "X" & quad4.textContent === "" || 
+        } else if (quad1.textContent === "X" && quad7.textContent === "X" & quad4.textContent === "" | 
         quad5.textContent === "X" && quad6.textContent === "X" && quad4.textContent === "") {
           roboChoice = "quad-4";
         } else if (quad7.textContent === "X" && quad9.textContent === "X" && quad8.textContent === "" || 
@@ -278,7 +312,8 @@ drEvil.addEventListener("click", () => {
         } else {
           roboChoice = choices[Math.floor(Math.random() * choices.length)];
         }
-        
+
+        console.log(roboChoice);
         let quad = document.querySelector(`#${roboChoice}`);
         if (quad.textContent != "") {
           roboChoice = "";
@@ -297,9 +332,9 @@ drEvil.addEventListener("click", () => {
       }
     }
   }
-  
-  
-  
+
+
+
 
   document.addEventListener("click", (e) => {
     if (!(e.target.closest(".quadrant"))) {
@@ -319,8 +354,8 @@ drEvil.addEventListener("click", () => {
       isRobot();
       isEvilRobot();
     }
-  }); 
-  return {gameOver};
-  
+  });
+  return { gameOver };
+
 })();
 
